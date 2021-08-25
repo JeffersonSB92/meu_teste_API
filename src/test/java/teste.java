9096a18f0c3454241051edfae2d5081f607e2c57
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-08-25 14:51:15 -0300

    Cria usuário com sucesso

--- src/test/java/teste.java
+++ src/test/java/teste.java
@@ -1,6 +1,8 @@
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -18,5 +20,17 @@ public class teste {
             body("data[0].email", is("michael.lawson@reqres.in")).
             body("data[0].first_name", is("Michael")).
             body("data", is(notNullValue()));
+    }
+
+    @Test
+    public void criaUsuarioComSucesso(){
+        given().log().all().
+            contentType(ContentType.JSON).
+            body("{\"name\": \"rafael\", \"job\": \"eng test\"}").
+        when().
+            post("https://reqres.in/api/users").
+        then().
+            statusCode(HttpStatus.SC_CREATED).
+            body("name", is("rafael"));
     }
 }
