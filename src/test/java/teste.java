a02c25c2bacd956ccdc5ac8c544fc64f0c967899
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-08-25 12:10:05 -0300

    Criado testes para listar metadados

--- src/test/java/teste.java
+++ src/test/java/teste.java
@@ -1,2 +1,22 @@
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.*;
+
 public class teste {
+
+    @Test
+    public void testeListaMetadadosDoUsuario(){
+        when().
+            get("https://reqres.in/api/users?page=2").
+        then().
+            statusCode(200). //alterar o valor para 203 ou outro para garantir que o teste falha
+            statusCode(HttpStatus.SC_OK).
+            body("page", is(2)). //alterar o valor para 1 ou outro para garantir que o teste falha
+            body("per_page", is(6)).
+            body("data[0].email", is("michael.lawson@reqres.in")).
+            body("data[0].first_name", is("Michael")).
+            body("data", is(notNullValue()));
+    }
 }
