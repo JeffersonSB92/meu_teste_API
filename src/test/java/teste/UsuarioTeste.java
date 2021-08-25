dff1550ef2bca0877675bc51a057a119d0e5bbcd
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-08-25 18:54:03 -0300

    adicionando baseURI e basePATH, e separando pacote teste e domínio

--- src/test/java/teste/UsuarioTeste.java
+++ src/test/java/teste/UsuarioTeste.java
@@ -1,27 +1,33 @@
+package teste;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.*;
 
-public class teste {
+public class UsuarioTeste {
 
     @BeforeAll
     public static void setup(){
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        baseURI = "https://reqres.in";
+        basePath = "/api";
     }
 
     @Test
     public void testeListaMetadadosDoUsuario(){
+        given().
+            params("page", "2").
         when().
-            get("https://reqres.in/api/users?page=2").
+            get("/users").
         then().
-            statusCode(200). //alterar o valor para 203 ou outro para garantir que o teste falha
+            statusCode(200). //alterar o valor para 203 ou outro para garantir que o UsuarioTeste.UsuarioTeste falha
             statusCode(HttpStatus.SC_OK).
-            body("page", is(2)). //alterar o valor para 1 ou outro para garantir que o teste falha
+            body("page", is(2)). //alterar o valor para 1 ou outro para garantir que o UsuarioTeste.UsuarioTeste falha
             body("per_page", is(6)).
             body("data[0].email", is("michael.lawson@reqres.in")).
             body("data[0].first_name", is("Michael")).
@@ -34,7 +40,7 @@ public class teste {
             contentType(ContentType.JSON).
             body("{\"name\": \"rafael\", \"job\": \"eng test\"}").
         when().
-            post("https://reqres.in/api/users").
+            post("/users").
         then().
             statusCode(HttpStatus.SC_CREATED).
             body("name", is("rafael"));
