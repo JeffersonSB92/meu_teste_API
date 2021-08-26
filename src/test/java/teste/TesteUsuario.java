4c6e55fb9181df929c0e8b23abc4c743b04835d0
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-08-26 16:34:21 -0300

    Renomeia nomenclaturas

--- src/test/java/teste/TesteUsuario.java
+++ src/test/java/teste/TesteUsuario.java
@@ -1,7 +1,6 @@
 package teste;
 
 import dominio.Usuario;
-import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +8,24 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class UsuarioTeste extends BaseTeste{
+public class TesteUsuario extends TesteBase {
 
     private static final String LISTA_USUARIOS_ENDPOINT = "/users";
     private static final String CRIA_USUARIO_ENDPOINT = "/user";
+
+    @Test
+    public void testeExemplos(){
+        given().
+            params("page", "2").
+        when().
+            get(LISTA_USUARIOS_ENDPOINT).
+        then().
+            statusCode(200). //alterar o valor para 203 ou outro para garantir que o TesteUsuario.TesteUsuario falha
+            statusCode(HttpStatus.SC_OK).
+            body("per_page", is(6)).
+            body("data[0].email", is("michael.lawson@reqres.in")).
+            body("data[0].first_name", is("Michael"));
+    }
 
     @Test
     public void testeListaMetadadosDoUsuario(){
@@ -21,17 +34,13 @@ public class UsuarioTeste extends BaseTeste{
         when().
             get(LISTA_USUARIOS_ENDPOINT).
         then().
-            statusCode(200). //alterar o valor para 203 ou outro para garantir que o UsuarioTeste.UsuarioTeste falha
             statusCode(HttpStatus.SC_OK).
-            body("page", is(2)). //alterar o valor para 1 ou outro para garantir que o UsuarioTeste.UsuarioTeste falha
-            body("per_page", is(6)).
-            body("data[0].email", is("michael.lawson@reqres.in")).
-            body("data[0].first_name", is("Michael")).
+            body("page", is(2)).
             body("data", is(notNullValue()));
     }
 
     @Test
-    public void criaUsuarioComSucesso(){
+    public void testeCriaUsuarioComSucesso(){
         Usuario usuario = new Usuario("rafael", "eng test", "usuario@gmail.com");
         given().
             body(usuario).
