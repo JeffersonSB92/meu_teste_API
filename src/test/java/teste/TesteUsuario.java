2fc7ba2eb4cc034315e9301a7684196fa7e51751
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-08-26 17:11:57 -0300

    Adiciona teste que checa tamanho do Data

--- src/test/java/teste/TesteUsuario.java
+++ src/test/java/teste/TesteUsuario.java
@@ -14,40 +14,59 @@ public class TesteUsuario extends TesteBase {
     private static final String CRIA_USUARIO_ENDPOINT = "/user";
 
     @Test
-    public void testeExemplos(){
+    public void testeExemplos() {
         given().
-            params("page", "2").
-        when().
-            get(LISTA_USUARIOS_ENDPOINT).
-        then().
-            statusCode(200). //alterar o valor para 203 ou outro para garantir que o TesteUsuario.TesteUsuario falha
-            statusCode(HttpStatus.SC_OK).
-            body("per_page", is(6)).
-            body("data[0].email", is("michael.lawson@reqres.in")).
-            body("data[0].first_name", is("Michael"));
+                params("page", "2").
+                when().
+                get(LISTA_USUARIOS_ENDPOINT).
+                then().
+                statusCode(200). //alterar o valor para 203 ou outro para garantir que o TesteUsuario.TesteUsuario falha
+                statusCode(HttpStatus.SC_OK).
+                body("per_page", is(6)).
+                body("data[0].email", is("michael.lawson@reqres.in")).
+                body("data[0].first_name", is("Michael"));
     }
 
     @Test
-    public void testeListaMetadadosDoUsuario(){
+    public void testeMostraPaginaEspecifica() {
         given().
-            params("page", "2").
-        when().
-            get(LISTA_USUARIOS_ENDPOINT).
-        then().
-            statusCode(HttpStatus.SC_OK).
-            body("page", is(2)).
-            body("data", is(notNullValue()));
+                params("page", "2").
+                when().
+                get(LISTA_USUARIOS_ENDPOINT).
+                then().
+                statusCode(HttpStatus.SC_OK).
+                body("page", is(2)).
+                body("data", is(notNullValue()));
     }
 
     @Test
-    public void testeCriaUsuarioComSucesso(){
+    public void testeCriaUsuarioComSucesso() {
         Usuario usuario = new Usuario("rafael", "eng test", "usuario@gmail.com");
         given().
-            body(usuario).
-        when().
-            post(CRIA_USUARIO_ENDPOINT).
-        then().
-            statusCode(HttpStatus.SC_CREATED).
-            body("name", is("rafael"));
+                body(usuario).
+                when().
+                post(CRIA_USUARIO_ENDPOINT).
+                then().
+                statusCode(HttpStatus.SC_CREATED).
+                body("name", is("rafael"));
+    }
+
+    @Test
+    public void testeTamanhoDosItensMostradosIgualAoPerPage() {
+        given().
+                params("page", "2").
+                when().
+                get(LISTA_USUARIOS_ENDPOINT).
+                then().
+                statusCode(HttpStatus.SC_OK).
+                body(
+                    "page", is(2),
+                    "data.size()", is(6),
+                    "data.findAll { it.avatar.startsWith('https://reqres.in')}.size()", is(6)
+//o código acima vai no data e acha todos(findAll), dentro das chaves ele
+//percorre todos itens(it) vendo se o avatar inicia com(startsWith) um valor específico
+//fora das chaves botamos o .size() para definir que é dentro do tamanho daquele array
+//e botamos ,is(6) para dizermos que têm de ser 6 posições nesse array
+                );
     }
 }
