ec5468ad7f99683d9f5475641403ce3ecb471939
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-08-26 23:19:45 -0300

    Adiciona ResponseSpecBuilder

--- src/test/java/teste/TesteBase.java
+++ src/test/java/teste/TesteBase.java
@@ -2,6 +2,7 @@ package teste;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -19,6 +20,11 @@ public class TesteBase {
     RestAssured.requestSpecification = new RequestSpecBuilder()
             .setContentType(ContentType.JSON)
             .build();
+
+    RestAssured.responseSpecification = new ResponseSpecBuilder()
+            .expectContentType(ContentType.JSON)
+            .build();
+
     }
 
 }
