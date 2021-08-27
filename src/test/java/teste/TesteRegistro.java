6d1fdf31394994ef8a12aca815436413551343c0
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2021-08-26 23:32:40 -0300

    Adiciona teste para Login não efetuado e responseSpecBuild na classe

--- src/test/java/teste/TesteRegistro.java
+++ src/test/java/teste/TesteRegistro.java
@@ -1,7 +1,11 @@
 package teste;
 
 import dominio.Usuario;
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,6 +14,14 @@ import static org.hamcrest.CoreMatchers.is;
 public class TesteRegistro extends TesteBase {
 
     private static final String REGISTRA_USUARIO_ENDPOINT = "/register";
+    private static final String LOGIN_USUARIO_ENDPOINT = "/login";
+
+    @BeforeAll
+    public static void setupRegistro(){
+        RestAssured.responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .build();
+    }
 
     @Test
     public void testeNaoEfetuaRegistroSemSenha(){
@@ -21,8 +33,20 @@ public class TesteRegistro extends TesteBase {
         when().
             post(REGISTRA_USUARIO_ENDPOINT).
         then().
-            statusCode(HttpStatus.SC_BAD_REQUEST).
             body("error", is("Missing password"));
+    }
+
+    @Test
+    public void testeLoginNaoEfetuadoQuandoSenhaEstaFaltando(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail("sydney@fife");
+
+        given().
+                body(usuario).
+                when().
+                post(LOGIN_USUARIO_ENDPOINT).
+                then().
+                body("error", is("Missing password"));
     }
 
 }
